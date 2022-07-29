e65a61392a155aad7d6013b12bd46192232abf3b
ejcalderongt <ejcalderon02@yahoo.com>
2022-07-28 20:03:54 -0600

    Initial commit

---
+++ app/src/main/java/com/posprinter/mpos_3nstar_driver/utils/Conts.java
@@ -0,0 +1,13 @@
+package com.posprinter.mpos_3nstar_driver.utils;
+
+public class Conts {
+	public static final int ENABLE_BLUETOOTH=1;
+	public static final int LABEL_PRINT=2;
+	public static final int type_moban1=5;
+	public static final int type_moban2=6;
+	public static final int type_moban3=7;
+	public static final int type_moban4=8;
+	public static final int TAKE_PICTURE=100;
+	public static final int LOAD_PICTURE_KITKAK=101;
+	public static final int LOAD_PICTURE=102;
+}
